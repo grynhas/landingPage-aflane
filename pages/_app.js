52530194c080be08756ca67f400f6c4dd4dd7c34
Patrick-Reis <git@ssh.dev.azure.com:v3/QsaudeDevOps/DigitalWorkPlace/App.E2E>
2021-03-27 16:28:05 -0300

    carrossel

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,8 @@ export default function App({ Component, pageProps }) {
         <title>Psi - Aflane Negrão</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
---
+++ src/components/Carrossel/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Carrossel = styled.div`
+    height: 250px;
+    width: auto;
+    padding:0 30px 0 30px;
+    min-width: 414px;
+    box-sizing: border-box;
+    margin-top: 20px;
+`
+export const Card = styled.div`
+    height: 250px;
+    width: auto;
+    background: pink;
+    display: flex !important;
+    justify-content: center;
+    align-items:center;
+`
